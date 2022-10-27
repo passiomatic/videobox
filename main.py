129048e58f934bfbc28d66164055944fe1890428
Andrea Peltrin <andrea.peltrin@omnys.com>
2022-10-27 16:12:10 +0200

    Split app and frame classes

--- main.py
+++ main.py
@@ -98,6 +98,51 @@ class ImageCache():
 cache = ImageCache()
 
 
+class MyDataStructure(object): 
+    """
+    Placeholder data structure
+    """
+    def __init__(self):
+        pass
+    
+    def load_data(self):
+        return []
+
+class AppController(wx.EvtHandler):
+	def __init__(self, frame, data):
+		self.frame = frame
+		self.data = data
+
+		# create child controls
+		self.button = wx.Button(self.frame, -1, "Load")
+		... 
+
+		# bind events
+		self.button.Bind(wx.EVT_BUTTON, self.OnLoadClicked)
+
+	def OnLoadClicked(self, event):
+		self.LoadData()
+
+	def LoadData(self):
+		self.data.load_data()
+		# populate wx.Frame controls with data
+
+
+class VideoboxApp(wx.App):
+	def OnInit(self):
+		frame = MainWindow(None, -1, "App Window")
+		data = MyDataStructure()
+		self.controller = AppController(frame, data)
+
+		# eventually...
+		# self.controller1 = DataViewController(frame, data)
+		# self.controller2 = AnimationController(frame)
+		# etc.
+
+		frame.Show()
+		return True
+
+
 def main():
 
     logging.basicConfig(level=configuration.log_level)
@@ -110,14 +155,12 @@ def main():
     cache.add(
         "https://www.thetvdb.com/banners/v4/series/401630/posters/614510da5fcb8.jpg")
 
-    # app = wx.App()
-    # ex = MainWindow(None)
-    # ex.Show()
-    # app.MainLoop()
+    # model.connect(shouldSetup=True)
+    # syncer = sync.Syncer()
+    # syncer.sync()
 
-    model.connect(shouldSetup=True)
-    syncer = sync.Syncer()
-    syncer.sync()
+    app = VideoboxApp()
+    app.MainLoop()
 
 if __name__ == '__main__':
     main()
