b5e52c7b57e2b605c1d5d160fe7a8f4d3e447e54
Andrea Peltrin <andrea@passiomatic.com>
2022-11-09 12:43:28 +0100

    Load images from cache is working

--- src/cache.py
+++ src/cache.py
@@ -17,42 +17,65 @@ class ImageCache(object):
         self.cache_dir = cache_dir
         self.download_queue = SimpleQueue()
         self.worker = Thread(target=self.worker, name="Cache worker", daemon=True)
-        #self.worker.start()
+        self.worker.start()
 
     def get(self, url, default):
+        digest = self.make_digest(url)
         try:
-            return self.images[url]
+            image = self.images[digest]
+            if image:
+                return image
+            else:
+                return default
         except KeyError:
-            # Schedule for fetching
-            self.download_queue.put(url)
-            logging.info("Could not find image {0} in cache, added to download queue".format(url))
-            return default
+            try:
+                # Lookup local cache 
+                image = self.load_image(digest)
+                self.set(digest, image)
+                return image
+            except FileNotFoundError:
+                # Schedule for fetching
+                self.download_queue.put(url)
+                logging.info("Could not find image {0} in cache, added to download queue".format(url))
+                return default
 
-    def set(self, url, image):
-        self.images[url] = image
+    def set(self, digest, image):
+        self.images[digest] = image
 
     def worker(self):
         while True: 
             url = self.download_queue.get()
             logging.debug("Fetching image {0}...".format(url))
             r = requests.get(url)            
+            digest = self.make_digest(url)
             if r.status_code == 200: 
                 data = BytesIO(r.content)
                 image = wx.Image(data)
-                self.set(url, image)
+                self.set(digest, image)
                 # @@TODO Or use r.headers['Content-Type']? 
-                self.store_image(url, r.content, image.GetType())
+                self.save_image(digest, r.content, image.GetType())
             else:
+                # Don't keep asking for broken images
+                self.set(digest, None)
                 logging.debug("Got status {0}, skipped".format(r.status_code))
     
-    def store_image(self, url, data, image_type):
-        # MD5 here since the filename is shorter and not collision-critical
-        filename = "{0}.{1}".format(hashlib.md5(url.encode("utf-8")).hexdigest(), self.get_image_extension(image_type))
-        with open(os.path.join(self.cache_dir, filename), "wb") as output:
-            output.write(data)
-            logging.debug("Stored new image {0}".format(filename))
-    
-    def get_image_extension(self, image_type):
+    def save_image(self, digest, data, image_type):
+        suffix = self.get_image_suffix(image_type)
+        if suffix:
+            filename = f"{digest}.{suffix}"
+            with open(os.path.join(self.cache_dir, filename), "wb") as output:
+                output.write(data)
+                logging.debug("Stored new image {0}".format(filename))
+
+    def load_image(self, digest):
+        # @@TODO Don't guess extension
+        filename = f"{digest}.jpg"
+        with open(os.path.join(self.cache_dir, filename), "rb") as input:
+            image = wx.Image(input)
+            logging.debug(f"Loaded image from cache {filename}")
+            return image
+
+    def get_image_suffix(self, image_type):
         if image_type == wx.BITMAP_TYPE_PNG:
             return "png"
         elif image_type == wx.BITMAP_TYPE_JPEG:
@@ -61,3 +84,6 @@ class ImageCache(object):
             logging.warn("Unrecognized image type {0}, skipped".format(image_type))
             return ""
 
+    def make_digest(self, url):
+        # MD5 here since the filename is shorter and not collision-critical
+        return hashlib.md5(url.encode("utf-8")).hexdigest()
