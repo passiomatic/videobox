085a6d293c585957c514b900ff43bf56f286c7d1
Andrea Peltrin <andrea@passiomatic.com>
2022-11-09 15:28:30 +0100

    Playing around with sidebar controls

--- src/views/sidebar.py
+++ src/views/sidebar.py
@@ -2,9 +2,11 @@ import wx
 #import logging
 import views.theme as theme
 from pubsub import pub
+import configuration
 
 # Messages 
 
+# Views
 
 class SidebarView(object):
     def __init__(self, image_cache, featured_series):
@@ -13,15 +15,34 @@ class SidebarView(object):
 
     def render(self, parent) -> wx.BoxSizer:
         box = wx.BoxSizer(wx.VERTICAL)
-        tree = wx.TreeCtrl(parent, wx.TR_HAS_BUTTONS)  
 
-        root = tree.AddRoot('Something goes here')
+        # TODO: Use wx.CollapsiblePane instead
+
+        # Series        
         
-        tree.SetPyData(root, ('key', 'value'))
-        tree.AppendItem(root, 'Some node value')
+        series_tree = wx.TreeCtrl(parent, wx.TR_HAS_BUTTONS)  
+        root = series_tree.AddRoot('Series')
+        
+        #series_tree.SetPyData(root, ('key', 'value'))
+        series_tree.AppendItem(root, 'Featured')
 
-        tree.Expand(root)
-        box.Add(tree, wx.EXPAND)
+        #series_tree.SetPyData(root, ('key', 'value'))
+        series_tree.AppendItem(root, 'Running')
+
+        series_tree.Expand(root)
+        box.Add(series_tree, proportion=1, flag=wx.EXPAND)
+
+        # Tags
+
+        tags_tree = wx.TreeCtrl(parent, wx.TR_HAS_BUTTONS)  
+        root = tags_tree.AddRoot('Tags')
+
+        for tag, translation in configuration.TAGS.items():
+            #tags_tree.SetPyData(root, ('key', 'value'))
+            tags_tree.AppendItem(root, translation)
+
+        tags_tree.Expand(root)
+        box.Add(tags_tree, proportion=1, flag=wx.EXPAND)
 
         return box
 
