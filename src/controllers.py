64d55690cc2b0bc400aff596c1bc44a8fdf18eb7
Andrea Peltrin <andrea@passiomatic.com>
2022-11-22 15:46:30 +0100

    Show featured series panel

---
+++ src/controllers.py
@@ -0,0 +1,36 @@
+import kivy
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
+from kivy.uix.image import AsyncImage
+from kivy.uix.behaviors import ButtonBehavior
+import logging
+
+
+class Home(GridLayout):
+    featured_series = ObjectProperty()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.update()
+
+    def update(self):
+        for series in self.featured_series:
+            self.add_widget(SeriesThumbnail(id=series.tvdb_id,
+                            poster_url=series.poster_url, label=series.name))
+
+
+class Imagebutton(ButtonBehavior, AsyncImage):
+    """
+    A clickable image loaded via HTTP
+    """
+    pass
+
+
+class SeriesThumbnail(BoxLayout):
+    id = NumericProperty()
+    poster_url = StringProperty()
+    label = StringProperty()
+
+    def show_series_detail(self):
+        logging.info(f"Clicked show_series_detail {self.id}")
--- src/main.py
+++ src/main.py
@@ -1,36 +1,19 @@
 import kivy
 kivy.require('2.1.0')
 
-from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
-
 import logging
 import configuration
 import sync
 import model
-#from cache import ImageCache
 import os
-#import views.theme as theme
 from pubsub import pub
 #import torrenter
 from dataclasses import dataclass
+import controllers
 
-
-class Controller(FloatLayout):
-    '''Create a controller that receives a custom widget from the kv lang file.
-
-    Add an action to be called from the kv lang file.
-    '''
-    label_wid = ObjectProperty()
-    info = StringProperty()
-
-    def do_action(self):
-        self.label_wid.text = 'My label after button press'
-        self.info = 'New info text'
-
-
-class ControllerApp(App):
+class VideoboxApp(App):
+    pass
 
     def build(self):
         self.sync_worker = None
@@ -55,7 +38,8 @@ class ControllerApp(App):
 
         model.connect(app_dir, shouldSetup=True)
 
-        return Controller(info='Hello world')
+        featured_series = model.get_featured_series(7)[:12]
+        return controllers.Home(featured_series=featured_series)
 
 
 if __name__ == '__main__':
@@ -65,4 +49,4 @@ if __name__ == '__main__':
         # Set higher log level for deps
         logging.getLogger(module).setLevel(logging.WARN)
 
-    ControllerApp().run()
+    VideoboxApp().run()
