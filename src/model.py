60886fb888800caa06c85ebb503af0acce676e52
Andrea Peltrin <andrea@passiomatic.com>
2022-12-05 12:18:15 +0100

    Update series tag list

--- src/model.py
+++ src/model.py
@@ -8,7 +8,7 @@ import sqlite3
 DATABASE_FILENAME = 'library.db'
 
 TAGS = {
-    "action": "Action", "adventure": "Adventure", "animation": "Animation", "anime": "Anime", "children": "Children", "comedy": "Comedy", "crime": "Crime", "documentary": "Documentary", "drama": "Drama", "family": "Family", "fantasy": "Fantasy", "food": "Food", "game-show": "Game Show", "home-and-garden": "Home and Garden", "horror": "Horror", "mini-series": "Mini-Series", "mystery": "Mystery", "news": "News", "reality": "Reality", "romance": "Romance", "science-fiction": "Science Fiction", "soap": "Soap", "special-interest": "Special Interest", "sport": "Sport", "suspense": "Suspense", "talk Show": "Talk Show", "thriller": "Thriller", "travel": "Travel", "western": "Western", "war": "War"
+    "action": "Action", "adventure": "Adventure", "animation": "Animation", "anime": "Anime", "awards-show": "Awards Show", "children": "Children", "comedy": "Comedy", "crime": "Crime", "documentary": "Documentary", "drama": "Drama", "family": "Family", "fantasy": "Fantasy", "food": "Food", "game-show": "Game Show", "history": "History", "home-and-garden": "Home and Garden", "horror": "Horror", "indie": "Indie", "martial-arts": "Martial Arts", "mini-series": "Mini-Series", "musical": "Musical", "mystery": "Mystery", "news": "News", "podcast": "Podcast", "reality": "Reality", "romance": "Romance", "science-fiction": "Science Fiction", "soap": "Soap", "special-interest": "Special Interest", "sport": "Sport", "suspense": "Suspense", "talk Show": "Talk Show", "thriller": "Thriller", "travel": "Travel", "western": "Western", "war": "War"
 }
 
 TORRENT_ADDED = "A"
