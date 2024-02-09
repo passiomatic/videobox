99280c284b7ba884390ead79df6f5d3a7173f114
Andrea Peltrin <andrea@passiomatic.com>
2024-02-09 10:30:35 +0100

    Add barebone tests for web interface pages

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -4,6 +4,7 @@ from videobox import create_app
 from videobox.models import db_wrapper
 from videobox import TestingConfig
 
+
 TEST_DIR = Path(__file__).parent
 
 @pytest.fixture()
@@ -25,7 +26,30 @@ def client(app):
 
 def test_home(client):  
     r = get(client, '/')
-    assert r.status_code == 200    
+    assert r.status_code == 200
+    # Check main homepage sections
+    assert b'Featured series' in r.data 
+    assert b'Popular tags' in r.data 
+    # Check footer
+    assert b'This is Videobox' in r.data 
+
+def test_tag_index(client):  
+    r = get(client, '/tag')
+    assert r.status_code == 200
+    # Check main categories
+    assert b'Action &amp; Adventure' in r.data 
+    assert b'Animation' in r.data 
+    assert b'Comedy' in r.data 
+
+def test_tag_detail(client):  
+    r = get(client, '/tag/action')
+    assert r.status_code == 200
+    assert b'Action &amp; Adventure' in r.data 
+
+# def test_search(client):  
+#     r = get(client, '/search', query_string={'query': 'simpsons'})
+#     assert r.status_code == 200
+#     assert b'The Simpsons' in r.data 
 
 # --------------
 #  Helpers 
