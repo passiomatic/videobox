3175adfa7b39fec04774a589dff12fb3a559eefd
Andrea Peltrin <andrea@passiomatic.com>
2024-08-05 15:48:07 +0200

    Display torrent client running status

--- videobox/main/routes.py
+++ videobox/main/routes.py
@@ -343,7 +343,15 @@ def system_status():
                            .where((Tracker.status << [models.TRACKER_OK, models.TRACKER_TIMED_OUT]))
                            .scalar())
     return flask.render_template("status.html", 
-                                 log_rows=log_rows, torrents=torrents, trackers=trackers, chart=chart_query, max_chart_days=MAX_CHART_DAYS, max_log_rows=MAX_LOG_ROWS, max_last_scraped_on=max_last_scraped_on)
+                                 log_rows=log_rows, 
+                                 torrents=torrents, 
+                                 trackers=trackers, 
+                                 chart=chart_query, 
+                                 max_chart_days=MAX_CHART_DAYS, 
+                                 max_log_rows=MAX_LOG_ROWS, 
+                                 max_last_scraped_on=max_last_scraped_on,
+                                 torrent_running=bt.torrent_worker.is_alive(),
+                                 torrent_port=app.config.get('TORRENT_PORT', ''))
 
 
 # ---------
