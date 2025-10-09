38a4df43f3bc7a971525df166f1ae9556bfa2255
Andrea Peltrin <andrea@passiomatic.com>
2025-10-09 17:53:42 +0200

    Add tests for non-ascii suggest query

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -70,7 +70,17 @@ def test_search_info_hash(client):
     r = client.get('/search', query_string={'query': release.info_hash})
     assert r.status_code == 302
 
-def test_suggest(client):  
+def test_suggest_with_ascii(client):  
     r = client.get('/suggest', query_string={'query': 'Simpsons'})
     assert r.status_code == 200
     assert b'The Simpsons' in r.data
+
+def test_suggest_with_unicode(client):  
+    r = client.get('/suggest', query_string={'query': 'Shōgun'})
+    assert r.status_code == 200
+    assert 'Shōgun' in r.text
+
+def test_suggest_with_unicode_2(client):  
+    r = client.get('/suggest', query_string={'query': 'Shogun'})
+    assert r.status_code == 200
+    assert 'Shōgun' in r.text
