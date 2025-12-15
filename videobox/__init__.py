2bf6b308866bb1ee6c56bd4878b55229d065110f
Andrea Peltrin <andrea@passiomatic.com>
2025-12-15 17:06:06 +0100

    Fish crash

--- videobox/__init__.py
+++ videobox/__init__.py
@@ -100,9 +100,10 @@ def create_app(app_dir=None, data_dir=None, config_class=None):
 
             # Only releases since previous sync (if any)
             if bt.torrent_worker and last_log:
-                releases = models.get_downloadable_releases(last_log.timestamp, 0)
+                releases = models.get_downloadable_releases(last_log.timestamp, resolution=0, size_sorting=None)
                 for release in releases:
-                    bt.torrent_worker.add_torrent(release)
+                    app.logger.debug(f"Adding release #{release.id} to torrent worker queue")
+                    # bt.torrent_worker.add_torrent(release)
 
         sync.sync_worker = sync.SyncWorker(app.config["API_CLIENT_ID"], done_callback=on_update_done)
 
